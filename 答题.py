24551b6d1cf5a568c0ab9d7e99db0c1e855f306d
Sciencerailgun20001 <49398644+Sciencerailgun20001@users.noreply.github.com>
2021-08-12 22:03:52 +0800

    Update 答题.py

--- 答题.py
+++ 答题.py
@@ -2,9 +2,6 @@ import pygame,sys,random,os
 from pygame.locals import*
 pygame.init()
 canvas=pygame.display.set_mode((1200,720))
-pygame.display.set_caption('答题')
-a = pygame.image.load("images/封面.jpg")
-s = pygame.transform.smoothscale(a, (1200,720))#平滑缩放图片
 wt = pygame.image.load("images/1.png")
 bingo = pygame.image.load("images/correct.png.png")
 error  = pygame.image.load("images/error.png")
@@ -31,9 +28,7 @@ def handleEvent():
                 x = event.pos[0]
                 y = event.pos[1]
                 if 1200>x>1000 and 660>y>600:
-                    pygame.quit()
-                    os.system('python 胜利.py')
-                    sys.exit()
+                    exit()
         
 def button():
     global isRight,que,wt
@@ -109,15 +104,12 @@ def comPaint():
     if isRight==True:
             canvas.blit(wt,(0,0))
             canvas.blit(bingo,(1000,620))
-            fillText('恭喜回答正确，请点击下一题，你对党的了解可真深刻',(50,520))
     elif isRight=='none':
         canvas.blit(wt,(0,0))
         canvas.blit(error,(1000,620))
-        fillText('请选择摁下上下左右答题',(400,620))
     elif isRight==False:
         canvas.blit(wt,(0,0))
         canvas.blit(error,(1000,620))
-        fillText('回答错误，请重试,同志',(400,620))
         
         
 canvas.blit(s,(0,0))
@@ -132,7 +124,6 @@ while True:
     if state == 'WIN':
         canvas.blit(wt,(0,0))
         canvas.blit(goahead,(1000,620))
-        fillText('恭喜完成了本次答题任务！请点击继续',(200,520))
         
     handleEvent()
     pygame.display.update()
